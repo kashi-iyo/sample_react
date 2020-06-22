fbd8abb4672b20f43ac75b027038ee85df33ccbb
kasi-iyo <snakeman143@gmail.com>
2020-06-22 21:26:58 +0900

    Homeコンポーネントの作成

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,6 +1,27 @@
--Jumbotron
--TableOfContenst/List
- -Items
-  -Preview/Thumbnail
- -ActiveItem
-  -Video
+import React, { Component } from 'react'
+import Jumbotoron from './Jumbotoron'
+
+class Home extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      course_modules: [
+        { id: 1, title: '1, ReactでRailsアプリケーションをセットアップ', description: 'lorem ipsum', active: false },
+        { id: 1, title: '2, RailsアプリケーションにReactを追加', description: 'lorem ipsum', active: false },
+        { id: 1, title: '3, Hello Worldアプリケーションの立ち上げ', description: 'lorem ipsum', active: false },
+        { id: 1, title: '4, アプリケーションにReact Router Domを追加する', description: 'lorem ipsum', active: false },
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <Jumbotoron />
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ app/javascript/components/Home/Jumbotoron.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+const Jumbotoron = () => {
+  return (
+    <section className="home-section--1">
+      <div className="container">
+        <div className="row">
+          <div className="col col-sm-12 col-md-5">
+            <div className="pt-4 mt-4">
+              <h1>見出し1</h1>
+              <p>─小見出し─</p>
+            </div>
+          </div>
+          <div className="col col-sm-12 col-md-5">
+            <div className="pt-4 mt-4 text-center">
+              <div className="bg-danger">aa</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Jumbotoron
